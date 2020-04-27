dcd6d82aca6402d32c5fe802b2d5e6337390370a
Advaith Bhat <advaithbhat9@gmail.com>
2020-04-27 10:44:59 +0000

    base: Blur behind powermenu
    * Idea taken from Decendents
    * Thanks to @SuperDroidBond For helping
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -128,6 +128,8 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Set;
 
+import com.android.systemui.ImageUtilities;
+
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
  * may show depending on whether the keyguard is showing, and whether the device
@@ -1751,7 +1753,10 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
             // Window initialization
             Window window = getWindow();
+            View v1 = window.getDecorView();
             window.requestFeature(Window.FEATURE_NO_TITLE);
+            Bitmap bittemp = ImageUtilities.blurImage(mContext, ImageUtilities.screenshotSurface(mContext));
+            Drawable background = new BitmapDrawable(mContext.getResources(), bittemp);
             // Inflate the decor view, so the attributes below are not overwritten by the theme.
             window.getDecorView();
             window.getAttributes().systemUiVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -1820,7 +1825,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                                 FrameLayout.LayoutParams.MATCH_PARENT,
                                 FrameLayout.LayoutParams.MATCH_PARENT);
                 panelContainer.addView(mPanelController.getPanelContent(), panelParams);
-                mBackgroundDrawable = mPanelController.getBackgroundDrawable();
+               //  mBackgroundDrawable = mPanelController.getBackgroundDrawable();
                 mScrimAlpha = 1f;
             }
         }
@@ -1851,6 +1856,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 mScrimAlpha = ScrimController.GRADIENT_SCRIM_ALPHA;
             }
             getWindow().setBackgroundDrawable(mBackgroundDrawable);
+            Bitmap bittemp = ImageUtilities.blurImage(mContext, ImageUtilities.screenshotSurface(mContext));
+            Drawable background = new BitmapDrawable(mContext.getResources(), bittemp);
+            getWindow().setBackgroundDrawable(background);
         }
 
         private void fixNavBarClipping() {
@@ -1925,10 +1933,10 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mShowing = true;
             mHadTopUi = mStatusBarWindowController.getForceHasTopUi();
             mStatusBarWindowController.setForceHasTopUi(true);
-            mBackgroundDrawable.setAlpha(0);
+            // mBackgroundDrawable.setAlpha(0);
             mGlobalActionsLayout.setTranslationX(mGlobalActionsLayout.getAnimationOffsetX());
             mGlobalActionsLayout.setTranslationY(mGlobalActionsLayout.getAnimationOffsetY());
-            mGlobalActionsLayout.setAlpha(0);
+            // mGlobalActionsLayout.setAlpha(0);
             mGlobalActionsLayout.animate()
                     .alpha(1)
                     .translationX(0)
@@ -1938,7 +1946,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((Float) animation.getAnimatedValue()
                                 * mScrimAlpha * 255);
-                        mBackgroundDrawable.setAlpha(alpha);
+                        // mBackgroundDrawable.setAlpha(alpha);
                     })
                     .start();
         }
@@ -1962,7 +1970,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((1f - (Float) animation.getAnimatedValue())
                                 * mScrimAlpha * 255);
-                        mBackgroundDrawable.setAlpha(alpha);
+                        // mBackgroundDrawable.setAlpha(alpha);
                     })
                     .start();
             dismissPanel();
