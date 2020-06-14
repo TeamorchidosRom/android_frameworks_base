omnirom/android_frameworks_base_old

base: SystemUI: make QS columns count configurable
And add infrastructure to add more settings and
distribute them to QS panel

PS4
we must explicitly hide the columns submenu

base: SystemUI: add column count 6

now that we have hide label

base: SystemUI: qs colums menu with checkboxes

base: SystemUI add landscape qs columns config

allow max 9 columns in landscape

Change-Id: I401cca347da1a94f352a9ffcb99f1409ddc4d008

packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -528,13 +528,18 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
     }
 
-    private final SpanSizeLookup mSizeLookup = new SpanSizeLookup() {
+    private class OmniSpanSizeLookup extends SpanSizeLookup {
+        private int mColumns = 3;
         @Override
         public int getSpanSize(int position) {
             final int type = getItemViewType(position);
-            return type == TYPE_EDIT || type == TYPE_DIVIDER || type == TYPE_HEADER ? 3 : 1;
+            return type == TYPE_EDIT || type == TYPE_DIVIDER || type == TYPE_HEADER ? mColumns : 1;
         }
-    };
+        public void setColumnCount(int columns) {
+            mColumns = columns;
+        }
+    }
+    private final OmniSpanSizeLookup mSizeLookup = new OmniSpanSizeLookup();
 
     private class TileItemDecoration extends ItemDecoration {
         private final Drawable mDrawable;
@@ -654,4 +659,8 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         public void onSwiped(ViewHolder viewHolder, int direction) {
         }
     };
+
+    public void setColumnCount(int columns) {
+        mSizeLookup.setColumnCount(columns);
+    }
 }

TeamorchidosRom/android_frameworks_base

Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/res/values/ppui_strings.xml

packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -516,13 +516,18 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
     }
 
-    private final SpanSizeLookup mSizeLookup = new SpanSizeLookup() {
+    private class CustomQsSpanSizeLookup extends SpanSizeLookup {
+        private int mColumns = 3;
         @Override
         public int getSpanSize(int position) {
             final int type = getItemViewType(position);
-            return type == TYPE_EDIT || type == TYPE_DIVIDER || type == TYPE_HEADER ? 3 : 1;
+            return type == TYPE_EDIT || type == TYPE_DIVIDER || type == TYPE_HEADER ? mColumns : 1;
         }
-    };
+        public void setColumnCount(int columns) {
+            mColumns = columns;
+        }
+    }
+    private final CustomQsSpanSizeLookup mSizeLookup = new CustomQsSpanSizeLookup();
 
     private class TileItemDecoration extends ItemDecoration {
         private final Drawable mDrawable;
@@ -642,4 +647,8 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         public void onSwiped(ViewHolder viewHolder, int direction) {
         }
     };
+
+    public void setColumnCount(int columns) {
+        mSizeLookup.setColumnCount(columns);
+    }
 }